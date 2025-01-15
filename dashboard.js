8bad790ec42d9c7886f4c23ef2c4076314d58e9a
Nicolas <nicolasmenezes2402@gmail.com>
2025-01-15 13:15:58 -0300

    Atualização na dashboard e ícones

---
+++ dashboard.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const sidebar = document.getElementById("sidebar");
+  
+    hamburger.addEventListener("click", () => {
+      sidebar.classList.toggle("visible");
+    });
+  });
+  
